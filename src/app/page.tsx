35160d58c854d0684e0950cad62c51a6f1170f76
Oswaldo <oswaldojsotto@gmail.com>
2025-04-28 14:41:53 -0400

    pc

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import Contact from "@/sections/Contact";
 import Footer from "@/sections/Footer";
 import Header from "@/sections/Header";
@@ -6,10 +8,15 @@ import Pricing from "@/sections/Pricing";
 import Showcase from "@/sections/Showcase";
 import Slider from "@/sections/Slider";
 import Testimonials from "@/sections/Testimonials";
+import { ReactLenis, useLenis } from "lenis/react";
 
 export default function Home() {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const lenis = useLenis(({ scroll }) => {
+    // called every scroll
+  });
   return (
-    <>
+    <ReactLenis root>
       <Header />
       <Hero />
       <Slider />
@@ -18,6 +25,6 @@ export default function Home() {
       <Testimonials />
       <Contact />
       <Footer />
-    </>
+    </ReactLenis>
   );
 }
